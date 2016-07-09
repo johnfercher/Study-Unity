3f3398620e7fae46370f268e9f7413d5d8b28dff
Johnathan Fercher <Johnathan Fercher>
2016-07-09 07:37:23 -0300

    Ball 3D

---
+++ Ball 3D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    public int mode;
+
+    private Vector3 plataformOffsetPos;
+    private Quaternion plataformOffsetRotation;
+
+    private Vector3 rdPersonOffsetPos;
+    private Quaternion rdPersonOffsetRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        mode = 0;
+
+        // plataform view
+        plataformOffsetPos = new Vector3(0.0f, 0.5f, -7.0f);
+        plataformOffsetRotation.eulerAngles = new Vector3(0.0f, 00.0f, 0.0f);
+
+        // 3rd person view
+        rdPersonOffsetPos = new Vector3(-5.0f, 5.0f, 0.0f);
+        rdPersonOffsetRotation.eulerAngles = new Vector3(45.0f, 90.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 1"))
+        {
+            if (mode == 0)
+                mode = 1;
+            else
+                mode = 0;
+        }
+
+        if (mode == 0)
+        {
+            transform.position = player.transform.position + plataformOffsetPos;
+            transform.rotation = plataformOffsetRotation;
+        }
+        else
+        if (mode == 1)
+        {
+            transform.position = player.transform.position + rdPersonOffsetPos;
+            transform.rotation = rdPersonOffsetRotation;
+        }
+    }
+}
---
+++ Ball 3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private bool grounded;
+    private bool jump;
+    private bool super_jump;
+    private float move_speed, move_jump;
+    private Vector3 movement;
+    private int mode;
+
+    private Rigidbody rb;
+    public Text win_text;
+	// Use this for initialization
+	void Start () {
+        super_jump = false;
+        mode = 0;
+        grounded = true;
+        jump = false;
+        move_speed = 8.0f;
+        move_jump = 700.0f;
+        movement = Vector3.zero;
+
+        rb = GetComponent<Rigidbody>();
+        win_text.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInputs();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyForces();
+    }
+
+    void HandleInputs()
+    {
+        if (grounded) { 
+            movement.x = Input.GetAxis("Horizontal");
+            //movement.z = Input.GetAxis("Vertical");
+
+            if (!jump && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0"))
+            {
+                jump = true;
+            }
+        }
+
+        if (Input.GetKeyDown("joystick button 1"))
+        {
+            if (mode == 0)
+                mode = 1;
+            else
+                mode = 0;
+        }
+    }
+
+    void ApplyForces()
+    {
+        if (mode == 0) { 
+            // Apply Movement from Inputs
+            rb.velocity = new Vector3(movement.x * move_speed, rb.velocity.y, 0.0f);
+
+            // Apply Jump, Player must be grounded
+            if (jump)
+            {
+                rb.AddForce(new Vector3(move_speed + 5 + Input.GetAxis("Horizontal") * move_speed, move_jump + Input.GetAxis("Vertical") * move_jump * 0.3f, 0.0f));
+                if (super_jump)
+                {
+                    rb.AddForce(new Vector3(move_speed + 5 + Input.GetAxis("Horizontal") * move_speed, move_jump + Input.GetAxis("Vertical") * move_jump * 0.3f, 0.0f));
+                }
+                //rb.AddForce(new Vector3(0.0f, move_jump, 0.0f));
+                jump = false;
+            }
+        }else
+        if (mode == 1)
+        {
+            // Apply Movement from Inputs
+            rb.velocity = new Vector3(0.0f, rb.velocity.y, -movement.x * move_speed);
+
+            // Apply Jump, Player must be grounded
+            if (jump)
+            {
+                rb.AddForce(new Vector3(0.0f, move_jump + Input.GetAxis("Vertical") * move_jump * 0.3f, move_speed + 5 + Input.GetAxis("Horizontal") * move_speed));
+                if (super_jump)
+                {
+                    rb.AddForce(new Vector3(0.0f, move_jump + Input.GetAxis("Vertical") * move_jump * 0.3f, move_speed + 5 + Input.GetAxis("Horizontal") * move_speed));
+                }
+                //rb.AddForce(new Vector3(0.0f, move_jump, 0.0f));
+                jump = false;
+            }
+        }
+
+        // Apply Gravity
+        rb.AddForce(new Vector3(0.0f, -1000.0f * Time.deltaTime, 0.0f));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor_Jump"))
+        {
+            super_jump = true;
+            Debug.Log("super jump");
+        }else {
+            super_jump = false;
+        }
+
+        if (collision.gameObject.CompareTag("Floor_Finish"))
+        {
+            win_text.text = "Stage 1: \n You Win !";
+        }
+        else
+        {
+            super_jump = false;
+        }
+        grounded = true;
+            //Debug.Log("Grounded");
+        //}
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        //grounded = true;
+        //if (collision.gameObject.CompareTag("Floor"))
+        //{
+            grounded = false;
+        //}
+
+        if (collision.gameObject.CompareTag("Floor_Jump"))
+        {
+            super_jump = false;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor_Jump"))
+        {
+            super_jump = true;
+            Debug.Log("super jump");
+        }
+        else
+        {
+            super_jump = false;
+        }
+        //if (collision.gameObject.CompareTag("Floor"))
+        //{
+        grounded = true;
+        //}
+    }
+
+}
+
